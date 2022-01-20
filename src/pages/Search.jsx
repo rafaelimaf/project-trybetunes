41bbdc98f0dc9441b0a2f0cf93983f0bf5337201
Rafael de Lima <rafaelima.fernandez@gmail.com>
2022-01-20 10:57:04 -0300

    :sparkles: Requisito 05
    Adiciona barra de pesquisa na rota search

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,53 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 export default class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      searchInput: '',
+      isSearchBtnDisabled: true,
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    }, this.ableSearchBtn);
+  }
+
+  ableSearchBtn = () => {
+    const { searchInput } = this.state;
+    const SEARCH_MIN_SIZE = 2;
+    if (searchInput.length >= SEARCH_MIN_SIZE) {
+      this.setState({
+        isSearchBtnDisabled: false,
+      });
+    }
+  }
+
   render() {
+    const { searchInput, isSearchBtnDisabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
         <h1>Search</h1>
+        <input
+          data-testid="search-artist-input"
+          placeholder="Pesquise por artista"
+          type="text"
+          name="searchInput"
+          value={ searchInput }
+          onChange={ this.handleChange }
+        />
+        <button
+          data-testid="search-artist-button"
+          type="submit"
+          disabled={ isSearchBtnDisabled }
+        >
+          Pesquisar
+
+        </button>
       </div>
     );
   }
