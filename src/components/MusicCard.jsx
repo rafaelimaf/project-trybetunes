34b3c6acc285071f8bc159d4021edcbfa2b7afcb
Rafael de Lima <rafaelima.fernandez@gmail.com>
2022-01-24 11:33:42 -0300

    :sparkles: Requisito 09

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { addSong } from '../services/favoriteSongsAPI';
+import { addSong, getFavoriteSongs, removeSong } from '../services/favoriteSongsAPI';
 
 export default class MusicCard extends Component {
   constructor() {
@@ -8,23 +8,65 @@ export default class MusicCard extends Component {
     this.state = {
       favoriteSongs: [],
       isLoading: false,
+      isChecked: false,
     };
   }
 
-  addFavoriteSong = async ({ target }) => {
+  componentDidMount() {
+    this.recoverFavSongs();
+  }
+
+  /**
+   * Requisito 09 resolvido a partir da referência do repositério do aluno Carlos Rosa
+   * Link do Repositório: https://github.com/tryber/sd-017-project-trybetunes/pull/111
+   */
+  recoverFavSongs = async () => {
+    this.setState({ isLoading: true });
+    const recoveredFavSongs = await getFavoriteSongs();
+    this.setState({
+      isLoading: false,
+      favoriteSongs: recoveredFavSongs,
+    }, () => {
+      /**
+       * Estrutura que foi usada de referência:
+       * Usando o método some para percorrer o estado favoriteSongs, é possível fazer uma
+       * simples verificação onde: caso a musica com o mesmo trackId estivesse no array de
+       * músicas favoritas, ela já inicia com o checked = true
+       */
+      const { favoriteSongs } = this.state;
+      const { music } = this.props;
+      const wasChecked = favoriteSongs.some((favMusic) => (
+        favMusic.trackId === music.trackId
+      ));
+      if (wasChecked) {
+        this.setState({
+          isChecked: true,
+        });
+      }
+    });
+  }
+
+  /**
+   * Substituição do target.name pelo event na callback da linha 84
+   */
+  addFavoriteSong = async (event, music) => {
     this.setState({
       isLoading: true,
+      isChecked: event.target.checked,
     });
-    const addedSong = await addSong(target.name);
+    if (event.target.checked) {
+      await addSong(music);
+    } else {
+      await removeSong(music);
+    }
     this.setState({
       isLoading: false,
     });
-    console.log(`${addedSong} adicionada aos favoritos`);
   }
 
   render() {
     const { music } = this.props;
-    const { isLoading } = this.state;
+    const { isLoading, isChecked } = this.state;
     return (
       <div>
         <p>{music.trackName}</p>
@@ -38,9 +80,9 @@ export default class MusicCard extends Component {
         <label htmlFor="favorite">
           <input
             data-testid={ `checkbox-music-${music.trackId}` }
-            name={ music.trackName }
             type="checkbox"
-            onChange={ this.addFavoriteSong }
+            onChange={ (event) => this.addFavoriteSong(event, music) }
+            checked={ isChecked }
           />
           {isLoading ? (
             <p>Carregando...</p>
