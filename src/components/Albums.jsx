1531817afdaf3454ab902a99813bc6cd32c9a885
Rafael de Lima <rafaelima.fernandez@gmail.com>
2022-05-30 19:59:38 -0300

    feat: refatora componente Albums e estilização com o arquivo Albums.css

--- src/components/Albums.jsx
+++ src/components/Albums.jsx
@@ -1,25 +1,53 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
+import '../styles/components/Albums.css';
 
 export default class Albums extends Component {
+  componentDidMount() {
+    Aos.init({
+      duration: 700,
+    });
+  }
+
   render() {
     const { albumsList } = this.props;
     return (
-      <div>
+      <div className="albums-container">
         {albumsList.map((album) => (
-          <div
+          <Link
             className="album-container"
+            data-aos="fade-down"
             key={ album.collectionId }
+            to={ `/album/${album.collectionId}` }
           >
-            <Link
-              data-testid={ `link-to-album-${album.collectionId}` }
-              to={ `/album/${album.collectionId}` }
-            >
-              <h2>{album.collectionName}</h2>
-            </Link>
-            <img src={ album.artworkUrl100 } alt={ album.collectionName } />
-          </div>
+            <div>
+              <img
+                className="album-image"
+                src={ album.artworkUrl100 }
+                alt={ album.collectionName }
+              />
+              <div className="album-link">
+                <Link
+                  className="album-name"
+                  data-testid={ `link-to-album-${album.collectionId}` }
+                  to={ `/album/${album.collectionId}` }
+                >
+                  <h2>{album.collectionName}</h2>
+                </Link>
+              </div>
+              <div className="album-description">
+                <h3 className="track-count">
+                  {`${album.trackCount} Tracks`}
+                </h3>
+                <h6 className="release-date">
+                  {`Release ${album.releaseDate}`}
+                </h6>
+              </div>
+            </div>
+          </Link>
         ))}
       </div>
     );
